fc790101accca36bdf7637862aa43f980ea01eef
GKuzin13 <yo.is.josh@gmail.com>
2021-05-03 00:26:56 +0300

    Renamed file

---
+++ src/components/GroupSelect/GroupSelect.js
@@ -0,0 +1,84 @@
+import { useEffect, useState } from "react";
+import { fetchPosts } from "../../services/postHandler";
+import groupList from "../../groups";
+import { ACTIONS } from "../../reducers/reducers";
+import Sidebar from "../Sidebar/Sidebar";
+import CreatePostButton from "../CreatePostButton/CreatePostButton";
+import "./GroupSelect.css";
+
+const HomeActions = ({
+  dispatch,
+  windowSize,
+  manageLoader,
+  loading,
+  currentUser,
+}) => {
+  const [value, setValue] = useState("all");
+
+  useEffect(() => {
+    const filterPosts = () => {
+      fetchPosts().then((posts) => {
+        const filteredPosts = posts.filter(
+          (post) => post.postSubGroup === value
+        );
+
+        dispatch({
+          type: ACTIONS.SET_DATA,
+          data: value === "all" ? posts : filteredPosts,
+        });
+      });
+
+      manageLoader(false);
+    };
+
+    filterPosts();
+
+    return () => {
+      dispatch({
+        type: ACTIONS.SET_DATA,
+        data: [],
+      });
+    };
+  }, [value, dispatch, manageLoader]);
+
+  const onValuechange = (e) => {
+    setValue(() => e.target.value);
+    manageLoader(true);
+  };
+
+  if (windowSize?.width > 600) {
+    return (
+      <Sidebar
+        onValuechange={onValuechange}
+        value={value}
+        groupList={groupList}
+        currentUser={currentUser}
+        loading={loading}
+      />
+    );
+  }
+
+  return (
+    <div className="group-select-main-ctn">
+      <div className="group-select">
+        <select
+          aria-label="Select a group"
+          value={value}
+          onChange={(e) => onValuechange(e)}
+        >
+          {groupList.map((group, i) => {
+            return (
+              <option value={group} key={i}>
+                {`/${group}`}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+
+      {currentUser && <CreatePostButton />}
+    </div>
+  );
+};
+
+export default HomeActions;
