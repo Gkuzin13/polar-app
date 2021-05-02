8ba833139cbe3104fef1742e4fe7d46480dc0791
GKuzin13 <yo.is.josh@gmail.com>
2021-05-03 00:22:11 +0300

    Moved AuthProvider to index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.css";
+import { AuthProvider } from "./services/Auth";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
+import "./index.css";
 
 TimeAgo.addDefaultLocale(en);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
