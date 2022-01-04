332e44bd992c8b18c4a5e54ff186c4c64805364a
GKuzin13 <yo.is.josh@gmail.com>
2022-01-04 11:13:40 +0200

    moved map function to parent component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,34 +1,27 @@
 import { useEffect, useState } from 'react';
+import ReactTimeAgo from 'react-time-ago';
 import { updateUpVote, updateDownVote } from '../../services/postHandler';
 import { updateSavedPosts } from '../../services/userDataHandler';
+import { ACTIONS } from '../../reducers/reducers';
 import { ArrowUpIcon } from '@heroicons/react/solid';
 import { ArrowDownIcon } from '@heroicons/react/solid';
 import { ChatAltIcon } from '@heroicons/react/solid';
 import { BookmarkIcon } from '@heroicons/react/solid';
 import { ExternalLinkIcon } from '@heroicons/react/solid';
-import { ACTIONS } from '../../reducers/reducers';
 import './Post.css';
-import ReactTimeAgo from 'react-time-ago';
 
-const Post = ({
-  dispatch,
-  postData,
-  manageLoginWindow,
-  currentUser,
-  userData,
-}) => {
+const Post = ({ dispatch, post, manageLoginWindow, currentUser, userData }) => {
   const [votedPosts, setVotedPosts] = useState([]);
   const [savedPosts, setSavedPosts] = useState([]);
 
+  const userVoteData = votedPosts.find((posts) => posts.postId === post.postId);
+  const userSavedPosts = savedPosts.find((posts) => posts === post.postId);
+
   useEffect(() => {
-    setSavedPosts(userData?.userSavedPosts);
-    setVotedPosts(userData?.userVotedPosts);
-
-    if (!currentUser) {
-      setSavedPosts([]);
-      setVotedPosts([]);
+    if (currentUser) {
+      setSavedPosts(userData.userSavedPosts);
+      setVotedPosts(userData.userVotedPosts);
     }
-
     return () => {
       setSavedPosts([]);
       setVotedPosts([]);
@@ -51,7 +44,6 @@ const Post = ({
         const newSavedPosts = [...savedPosts].filter(
           (post) => post !== thisPost
         );
-
         setSavedPosts(() => newSavedPosts);
       });
     }
@@ -62,7 +54,8 @@ const Post = ({
       manageLoginWindow(true);
       return;
     }
-    if (typeof postVoteData === 'undefined') {
+
+    if (postVoteData === undefined) {
       postVoteData = {
         upVoted: false,
         downVoted: false,
@@ -83,7 +76,6 @@ const Post = ({
     const updatedVotedPosts = [...filteredPosts, toggledVote];
 
     updateUpVote(thisPost, postVoteData, currentUser.uid, toggledVote);
-
     setVotedPosts(updatedVotedPosts);
 
     if (postVoteData.downVoted) {
@@ -91,7 +83,6 @@ const Post = ({
         type: ACTIONS.UPVOTE_FROM_DOWNVOTE,
         payload: { id: thisPost.postId, user: currentUser.uid },
       });
-
       return;
     }
 
@@ -100,7 +91,6 @@ const Post = ({
         type: ACTIONS.DOWNVOTE_POST,
         payload: { id: thisPost.postId, user: currentUser.uid },
       });
-
       return;
     }
 
@@ -115,7 +105,7 @@ const Post = ({
       manageLoginWindow(true);
       return;
     }
-    if (typeof postVoteData === 'undefined') {
+    if (postVoteData === undefined) {
       postVoteData = {
         upVoted: false,
         downVoted: false,
@@ -136,7 +126,6 @@ const Post = ({
     const updatedVotedPosts = [...filteredPosts, toggledVote];
 
     updateDownVote(thisPost, postVoteData, currentUser.uid, toggledVote);
-
     setVotedPosts(updatedVotedPosts);
 
     if (postVoteData.upVoted) {
@@ -144,7 +133,6 @@ const Post = ({
         type: ACTIONS.DOWNVOTE_FROM_UPVOTE,
         payload: { id: thisPost.postId, user: currentUser.uid },
       });
-
       return;
     }
 
@@ -153,7 +141,6 @@ const Post = ({
         type: ACTIONS.UPVOTE_POST,
         payload: { id: thisPost.postId, user: currentUser.uid },
       });
-
       return;
     }
 
@@ -165,122 +152,86 @@ const Post = ({
 
   return (
     <div className='post-ctn'>
-      {postData.map((post) => {
-        const userVoteData = () => {
-          if (!votedPosts) {
-            return;
-          }
-
-          return votedPosts.find((posts) => posts.postId === post.postId);
-        };
-
-        const userSavedPosts = () => {
-          if (!savedPosts) {
-            return;
-          }
-
-          return savedPosts.find((posts) => posts === post.postId);
-        };
-
-        return (
-          <div key={post.postId} className='post-inner-ctn'>
-            {/* Post Main Info */}
-            <a
-              aria-label={`Show comments for this post, 
-                with the title ${post.postTitle}. Created by ${post.postOwner} `}
-              href={`/g/${post.postSubGroup}/${post.postId}`}>
-              <div className='post-info-ctn'>
-                <span className='post-info-group'>{`g/${post.postSubGroup}`}</span>
-                <div className='op-info'>
-                  <span>
-                    Posted by <strong>{post.postOwner}</strong>
-                  </span>
-                  <div className='post-date'>
-                    <ReactTimeAgo date={post.postDate} />
-                  </div>
-                </div>
+      <div className='post-inner-ctn'>
+        {/* Post Main Info */}
+        <a
+          aria-label={`Show comments for this post, with the title ${post.postTitle}. Created by ${post.postOwner} `}
+          href={`/g/${post.postSubGroup}/${post.postId}`}>
+          <div className='post-info-ctn'>
+            <span className='post-info-group'>{`g/${post.postSubGroup}`}</span>
+            <div className='op-info'>
+              <span>
+                Posted by <strong>{post.postOwner}</strong>
+              </span>
+              <div className='post-date'>
+                <ReactTimeAgo date={post.postDate} />
               </div>
-            </a>
-            {/* Post Content */}
-
-            <div className='post-content-ctn'>
-              <a
-                aria-label={`Show comments for this post, 
-                with the title ${post.postTitle}. Created by ${post.postOwner} `}
-                href={`/g/${post.postSubGroup}/${post.postId}`}>
-                <h1 className='post-title'>{post.postTitle}</h1>
-                <p>{post.postContent}</p>
-              </a>
-
-              {/* Checks if url exists */}
-              {!post.postContentUrl ? null : (
-                <a href={post.postContentUrl} className='post-content-url'>
-                  {post.postContentUrl.length > 60
-                    ? `${post.postContentUrl.slice(0, 60)}...`
-                    : post.postContentUrl}
-                  <ExternalLinkIcon className='url-icon' />
-                </a>
-              )}
-            </div>
-
-            {/* Post Actions */}
-            <div className='post-actions-ctn'>
-              <div className='votes-ctn'>
-                <ArrowUpIcon
-                  style={
-                    userVoteData()?.upVoted
-                      ? { color: 'rgb(251, 86, 7)' }
-                      : null
-                  }
-                  className='icon arrow-up-icon'
-                  onClick={() => toggleUpVote(post, userVoteData())}
-                />
-
-                <span>{post.postVotes}</span>
-
-                <ArrowDownIcon
-                  style={
-                    userVoteData()?.downVoted
-                      ? { color: 'rgb(58, 134, 255)' }
-                      : null
-                  }
-                  className='icon arrow-down-icon'
-                  onClick={() => toggleDownVote(post, userVoteData())}
-                />
-              </div>
-
-              <a
-                aria-label={`Show comments for this post, 
-                with the title ${post.postTitle}. Created by ${post.postOwner} `}
-                href={`/g/${post.postSubGroup}/${post.postId}`}>
-                <div className='post-comments-ctn'>
-                  <ChatAltIcon className='icon' />
-
-                  <span>
-                    {post.postComments
-                      ? Object.values(post.postComments).length
-                      : 0}
-                  </span>
-                  <span>Comments</span>
-                </div>
-              </a>
-
-              {currentUser && (
-                <div
-                  style={
-                    userSavedPosts() ? { color: 'rgb(44, 125, 102)' } : null
-                  }
-                  onClick={() => toggleSavePost(userSavedPosts(), post.postId)}
-                  className='post-save-ctn'>
-                  <BookmarkIcon className='icon' />
-
-                  <span>Save</span>
-                </div>
-              )}
             </div>
           </div>
-        );
-      })}
+        </a>
+        {/* Post Content */}
+        <div className='post-content-ctn'>
+          <a
+            aria-label={`Show comments for this post, 
+                with the title ${post.postTitle}. Created by ${post.postOwner} `}
+            href={`/g/${post.postSubGroup}/${post.postId}`}>
+            <h1 className='post-title'>{post.postTitle}</h1>
+            <p>{post.postContent}</p>
+          </a>
+          {/* Checks if url exists */}
+          {!post.postContentUrl ? null : (
+            <a href={post.postContentUrl} className='post-content-url'>
+              {post.postContentUrl.length > 60
+                ? `${post.postContentUrl.slice(0, 60)}...`
+                : post.postContentUrl}
+              <ExternalLinkIcon className='url-icon' />
+            </a>
+          )}
+        </div>
+        {/* Post Actions */}
+        <div className='post-actions-ctn'>
+          <div className='votes-ctn'>
+            <ArrowUpIcon
+              style={{ color: userVoteData?.upVoted && 'rgb(251, 86, 7)' }}
+              className='icon arrow-up-icon'
+              onClick={() => toggleUpVote(post, userVoteData)}
+            />
+            <span>{post.postVotes}</span>
+            <ArrowDownIcon
+              style={{
+                color: userVoteData?.downVoted && 'rgb(58, 134, 255)',
+              }}
+              className='icon arrow-down-icon'
+              onClick={() => toggleDownVote(post, userVoteData)}
+            />
+          </div>
+
+          <a
+            aria-label={`Show comments for this post, 
+                with the title ${post.postTitle}. Created by ${post.postOwner} `}
+            href={`/g/${post.postSubGroup}/${post.postId}`}>
+            <div className='post-comments-ctn'>
+              <ChatAltIcon className='icon' />
+              <span>
+                {post.postComments
+                  ? Object.values(post.postComments).length
+                  : 0}
+              </span>
+              <span>Comments</span>
+            </div>
+          </a>
+
+          {currentUser && (
+            <div
+              style={userSavedPosts && { color: 'rgb(44, 125, 102)' }}
+              onClick={() => toggleSavePost(userSavedPosts, post.postId)}
+              className='post-save-ctn'>
+              <BookmarkIcon className='icon' />
+              <span>Save</span>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
